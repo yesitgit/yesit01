76921d19d680d6d7739c77aef14ecdb471d661b3
root <root@localhost.localdomain>
2018-10-19 12:40:30 +0530

    Docker Setup Committed Successfully.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS') # new
+app.config.from_object(app_settings) # new
+
+import sys
+print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
